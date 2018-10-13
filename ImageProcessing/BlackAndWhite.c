ad706516928e536a1821d4216456203acd08f1c7
Pyrd <mathieu.cailly@gmail.com>
2018-10-13 18:06:45 +0200

    Adding BlackAndWhite class

---
+++ ImageProcessing/BlackAndWhite.c
@@ -0,0 +1,5 @@
+//
+// Created by pyrd on 13/10/18.
+//
+
+#include "BlackAndWhite.h"
---
+++ ImageProcessing/BlackAndWhite.h
@@ -0,0 +1,8 @@
+//
+// Created by pyrd on 13/10/18.
+//
+
+#ifndef OCR_PROJECT_BLACKANDWHITE_H
+#define OCR_PROJECT_BLACKANDWHITE_H
+
+#endif //OCR_PROJECT_BLACKANDWHITE_H
