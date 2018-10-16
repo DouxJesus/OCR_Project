08c568570b801259784f4d051e0d738309b8949a
Marine Charra <marine.charra@epita.fr>
2018-10-16 09:49:03 +0200

    ajout POC

---
+++ POC/cmake-build-debug/CMakeFiles/POC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POC.dir/main.c.o"
+  "POC.pdb"
+  "POC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
