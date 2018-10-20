7ccb7aa4d6cff9279af3b43d598ec094fe20a61d
Pyrd <mathieu.cailly@gmail.com>
2018-10-20 17:00:04 +0200

    blblbl -- working on RSLA
    Signed-off-by: Pyrd <mathieu.cailly@gmail.com>

--- ImageProcessing/RSLA.c
+++ ImageProcessing/RSLA.c
@@ -22,39 +22,107 @@ SDL_Surface* RSLA(SDL_Surface *img){
 //horizontal -> bool -> define if do horizontal pass (true) or vertical pass(false)
 SDL_Surface* Process(SDL_Surface *img, int threshold ,int horizontal){
 	SDL_Surface *output = NULL;
-
+	output = InitSurfaceFromAnother(img, output); //Init output with img's dimension
 	int width, height;
 	width = img->w;
 	height= img->h;
 	output.h = height;
 	output.w = width;
 
+
+	//If the surface must be locked
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        //Lock the surface
+        SDL_LockSurface( surface );
+    }
+
+
 	if(horizontal){
 		//DO HORIZONTAL PASS
-		//Go through columns
-    	for( int x = 0, rx = output->w - 1; x < output->w; x++, rx-- )
-    	{
-        	//Go through rows
-        	for( int y = 0, ry = output->h - 1; y < output->h; y++, ry-- )
-        	{
-            	//Get pixel
+    	//Go through rows
+        for( int y = 0; y < output->h; y++)
+        {
+        	//Go through columns
+			for( int x = 0; x < output->w; x++ )
+   			{
+        		//Get pixel
             	Uint32 pixel = get_pixel(img, x, y);
             	Uint8 r, g, b;
-				SDL_GetRGB(pixel, image_surface->format, &r, &g, &b);
-            	if(r > 127 && g > 127 && b > 127){
+				SDL_GetRGB(pixel, img->format, &r, &g, &b);
+            	if(r < 127 && g < 127 && b < 127){
             		//Pixel is black
+            		put_pixel(output, x, y, pixel)
             	}
             	else{
             		//Pixel is White
-            	}
+            		//Check if there is less or equal than threshold adjacent white pixels
+            		int left_adjacent_pix = 0;
+            		int index = x - 1;
+            		int goOn = 1;
+            		//Check on left side adjacent pix
+            		while(index >= 0 && goOn == 1){
+            			if(left_adjacent_pix > threshold){
+            				//TOOMUCH -> stop
+            				left_adjacent_pix = -1;
+            				goOn = 0;
+            			}
+            			Uint32 c_pixel = get_pixel(img, index, y);
+            			Uint8 cr,cg, cb;
+						SDL_GetRGB(c_pixel, img->format, &cr, &cg, &cb);
+            			if(cr > 127 && cg > 127 && cb > 127){
+            				//Pixel is white
+            				left_adjacent_pix++;
+            			}
+            			else
+            			{
+            				//Pixel is black -> stop
+            				goOn = 0;
+            			}
+            			index--;
+            		}
 
-            	
-        	}
-    	}
+            		index = x + 1;
+            		goOn = 1;
+            		int right_adjacent_pix = 0;
+            		while(index < width - 1 && goOn == 1){
+            			if(right_adjacent_pix > threshold){
+            				//TOOMUCH -> stop
+            				left_adjacent_pix = -1;
+            				goOn = 0;
+            			}
+            			Uint32 c_pixel = get_pixel(img, index, y);
+            			Uint8 cr,cg, cb;
+						SDL_GetRGB(c_pixel, img->format, &cr, &cg, &cb);
+            			if(cr > 127 && cg > 127 && cb > 127){
+            				//Pixel is white
+            				right_adjacent_pix++;
+            			}
+            			else
+            			{
+            				//Pixel is black -> stop
+            				goOn = 0;
+            			}
+            			index--;
+            		}
+
+            	}
+   			}	
+       	}
 	}
 	else {
+		//NOT FINISHED
 		//DO VERTICAL PASS
+
 	}
+
+	//Unlock surface
+    if( SDL_MUSTLOCK( surface ) )
+    {
+        SDL_UnlockSurface( surface );
+    }
+
+	return output;
 
 }
 
